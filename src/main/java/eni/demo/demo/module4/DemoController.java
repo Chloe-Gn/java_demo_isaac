57b3269b301e322fd1d57995622418d5a8d4d6d6
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-08-20 12:27:01 +0200

    Demo front 1

--- src/main/java/eni/demo/demo/module4/DemoController.java
+++ src/main/java/eni/demo/demo/module4/DemoController.java
@@ -9,6 +9,21 @@ public class DemoController {
     @GetMapping("chocolatine")
     public String chocolatine() {
 
+        // Retourne le nom du fichier HTML à charger
+        // PS: A partir du dossier resources/templates
+        // resources/templates est la racine des fichiers HTML
         return "todo";
     }
+
+    // Dans l'annotion XXXMapping, le première paramètre => le nom de l'url
+    // exemple "login" => http://monhost:monport/login
+    // donc en localhost avec le port 8080 par défaut (en dev) => http://localhost:8080/login
+    @GetMapping("login")
+    public String showLoginForm(){
+        // Retourne le nom du fichier HTML à charger
+        // PS: A partir du dossier resources/templates
+        // resources/templates est la racine des fichiers HTML
+        return "login-page";
+    }
+
 }
