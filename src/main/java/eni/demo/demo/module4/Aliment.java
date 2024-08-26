ceff361cb636d9355ceb16302ee197b4ab1eb920
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-08-26 12:19:07 +0200

    demo suite

--- src/main/java/eni/demo/demo/module4/Aliment.java
+++ src/main/java/eni/demo/demo/module4/Aliment.java
@@ -5,8 +5,23 @@ public class Aliment {
     public long id;
     public String name;
 
+    public Aliment() {
+    }
+
     public Aliment(long id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
---
+++ src/main/java/eni/demo/demo/module4/DemoFormController.java
@@ -0,0 +1,35 @@
+package eni.demo.demo.module4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DemoFormController {
+
+    @GetMapping("show-aliment-form")
+    public String showAlimentForm(Model model) {
+
+        // Préparer ce que tu va envoyer dans le formulaire par défaut
+        Aliment aliment = new Aliment(-1L, "");
+
+        // Envoyer l'aliment dans le front (dans la réponse)
+        // pour le mettre dans le formulaire
+        model.addAttribute("aliment", aliment);
+
+        // Afficher la page formulaire
+        return "form/form-aliment-page";
+    }
+
+    /**
+     * Traiter les données qui nous serons envoyé
+     * @return
+     */
+    @PostMapping("aliment-form")
+    public String alimentForm(@ModelAttribute Aliment aliment) {
+        // Afficher la page formulaire
+        return "form/form-aliment-page";
+    }
+}
